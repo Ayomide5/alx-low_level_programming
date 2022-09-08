6bc4ab620a06dee45bfae264da5cc9b4b6a8cced
Ayomide5 <mdakindele@gmail.com>
2022-09-08 12:58:38 -0700

    Mycommit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
 
 
 /**
+  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   *
+ *   
  *
  *    * Return: 0 if exited properly, non-zero otherwise
  *
@@ -18,6 +17,5 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-
+		return (0);
 }
